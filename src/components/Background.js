490c21bc1155239d7ff001858ee281d1e8b92250
Your name <omowunmidaud1@gmail.com>
2023-12-27 22:12:59 +0100

    updated the landing page

---
+++ src/components/Background.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Background = () => {
+  return (
+    <div className='background'>
+
+        <header>
+
+            <Image src='/images/fitstarklogo.png' alt='' width='150' height='50' />
+
+            <div className='navlinks'>
+
+              <div>
+                <Image src='/images/facebook.png' alt='' width='30' height='50' />
+                <Image src='/images/x.png' alt='' width='30' height='50' />
+                <Image src='/images/instagram.png' alt='' width='30' height='50' />
+                <Image src='/images/linkedin.png' alt='' width='30' height='50' />
+              </div>
+
+              <p>connect with Us</p>
+
+            </div>
+        </header>
+
+    </div>
+  )
+}
+
+export {Background}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,56 @@
 import Image from "next/image";
 import Form from "./WaitForm";
+import '../../styles/background.css'
+import Link from 'next/link'
 
 export default function Main() {
   return (
-    <div className="bg-[#293747] min-h-screen h-full flex flex-wrap">
-      <LeftSide />
-      <RightSide />
+
+    <div className='background'>
+
+      <header>
+
+          <Image src='/images/fitstarklogo.png' alt='' width='150' height='50' />
+
+          <div className='navlinks'>
+
+            <div>
+
+              <Image src='/images/facebook.png' alt='' width='30' height='50' />
+              <Image src='/images/x.png' alt='' width='30' height='50' />
+              <Image src='/images/instagram.png' alt='' width='30' height='50' />
+              <Image src='/images/linkedin.png' alt='' width='30' height='50' />
+
+            </div>
+
+            <p>connect with Us</p>
+
+          </div>
+
+      </header>
+
+      <div className="main-content">
+        
+        <h1>Embracing complete wholeness.</h1>
+        <p id="p1">FitStark is launching in 2024!</p>
+        <p id="p2">Be the first to know when we are done cooking your tailored meals and workouts,
+           <span> embracing</span> complete wholeness!
+        </p>
+        <Link href="/waitlist" id="button">JOIN WAITLIST</Link>
+
+      </div>
+
+      {/*<div className="bg-[#293747] min-h-screen h-full flex flex-wrap">
+        <LeftSide />
+        <RightSide />
+      </div> */}
+
     </div>
+
   );
 }
 
-function LeftSide() {
+{/* function LeftSide() {
   return (
     <div className="flex-1 m-auto p-8 md:flex-[50%]">
       <Image className="py-6" width="250" height="30" src="/christmasLogo.png" alt="logo" />
@@ -39,4 +79,4 @@ function RightSide() {
       />
     </div>
   );
-}
+} */}
