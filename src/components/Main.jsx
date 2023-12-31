4bd1a486f77897b252896d9c27d26b3c9edeb432
Your name <omowunmidaud1@gmail.com>
2023-12-31 11:11:52 +0100

    completed the page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,87 @@
+"use client";
+import React, { useState } from 'react';
 import Image from "next/image";
 import Form from "./WaitForm";
 import '../../styles/background.css'
 import Link from 'next/link'
 
 export default function Main() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleJoinWaitlistClick = () => {
+    setShowForm(true);
+  };
+
+  const handleCloseFormClick = () => {
+    setShowForm(false);
+  };
+
   return (
 
     <div className='background'>
 
-      <header>
+        <header>
+          {/* ... Header content ... */}
+          <Link href="/" id="">
+            <Image src='/images/fitstarklogo.png' alt='' width='150' height='50' />
+          </Link>
 
-          <Image src='/images/fitstarklogo.png' alt='' width='150' height='50' />
+          <Image src='/images/Frame (2).png' alt='' width='30' height='50' id="hamburger" />
+
+          <div className='navlinks'>
+
+            <div>
+
+              <Image src='/images/facebook.png' alt='' width='30' height='30' />
+              <Image src='/images/x.png' alt='' width='30' height='30' />
+              <Image src='/images/instagram.png' alt='' width='30' height='30' />
+              <Image src='/images/linkedin.png' alt='' width='30' height='30' />
+
+            </div>
+
+            <p style={{color: '#fff', fontSize: '15px'}}>connect with Us</p>
+
+          </div>
+
+        </header>
+
+        {showForm ? (
+
+          <div className='form-container'>
+  
+            <Image src='/images/Frame.png' alt='' width='30' height='50' id='close' onClick={handleCloseFormClick} />
+
+              <h1>Enter your details to Get Eerly Access</h1>
+
+            <Form />
+
+          </div>
+        ) : (
+
+          <>
+        
+            <div className="main-content">
+        
+              <h1>Embracing complete wholeness.</h1>
+              <p id="p1">FitStark is launching in 2024!</p>
+              <p id="p2">Be the first to know when we are done cooking your tailored meals and workouts,
+                 <span> embracing</span> complete wholeness!
+              </p>
+
+              <button id="button" onClick={handleJoinWaitlistClick}> JOIN WAITLIST </button>
+              
+            </div>
+          </>
+
+        )}
+
+     {/* <header>
+          
+          <Link href="/" id="">
+            <Image src='/images/fitstarklogo.png' alt='' width='150' height='50' />
+          </Link>
+
+          <Image src='/images/Frame (2).png' alt='' width='30' height='50' id="hamburger" />
 
           <div className='navlinks'>
 
@@ -40,9 +111,14 @@ export default function Main() {
 
       </div>
 
-      {/*<div className="bg-[#293747] min-h-screen h-full flex flex-wrap">
-        <LeftSide />
-        <RightSide />
+      <div className='form-container'>
+  
+        <Image src='/images/Frame.png' alt='' width='30' height='50' id='close' />
+
+        <h1>Enter your details to Get Eerly Access</h1>
+
+        <Form />
+              
       </div> */}
 
     </div>
@@ -65,7 +141,13 @@ export default function Main() {
       <Form />
     </div>
   );
-}
+
+
+
+      {/*<div className="bg-[#293747] min-h-screen h-full flex flex-wrap">
+        <LeftSide />
+        <RightSide />
+      </div> 
 
 function RightSide() {
   return (
--- src/components/WaitForm.jsx
+++ src/components/WaitForm.jsx
@@ -1,8 +1,9 @@
 "use client";
 import { useRef, useState } from "react";
+import Image from "next/image";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [userType, setUserType] = useState("");
   const [email, setEmail] = useState("");
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [error, setError] = useState(null);
@@ -20,43 +21,69 @@ export default function Form() {
 
   if (hasSubmitted) {
     return (
-      <div className="pt-12">
-        <span className="text-lg font-light text-white">
-          Thanks for signing up! We will be in touch soon.
-        </span>
+      <div className="p-8">
+
+      <div style={{color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: '20px'}}>
+
+        <Image src='/images/Frame (1).png' alt='' width='50' height='50' />
+        <p>Your interest has been successfully received!</p>
+        <p>You will be notified duly!</p>
+        <p>Anticipate our launch in 2024!</p>
+
+        <div className='navlinks' style={{width: '30%', marginTop: '1em'}}>
+
+          <div>
+
+            <Image src='/images/X svg.png' alt='' width='30' height='30' />
+            <Image src='/images/Facebook svg.png' alt='' width='30' height='30' />
+            <Image src='/images/Instagram svg.png' alt='' width='30' height='30' />
+            <Image src='/images/Linkedin svg.png' alt='' width='30' height='30' />
+
+          </div>
+
+          <p style={{color: 'black', fontSize: '15px'}}>connect with Us</p>
+
+        </div>
+        
+      </div>
       </div>
     );
   }
 
   return (
     <>
-      <form ref={form} className="pt-12 flex flex-wrap" onSubmit={submit}>
-        <input
-          type="name"
-          name="name"
-          required
-          placeholder="Name"
-          className="p-3 m-2 box-border border-none rounded-lg text-lg flex-1 bg-[#232323] text-white mr-2"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          autoComplete="name"
-        />
+      <form ref={form} className="waitform" onSubmit={submit}>
+
         <input
           type="email"
           name="email"
           required
-          placeholder="Email"
-          className="p-3 m-2 box-border border-none rounded-lg text-lg flex-1 bg-[#232323] text-white mr-2"
+          placeholder="Email email address"
+          className="p-2 m-2 box-border rounded-lg text-sm flex-1"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           autoComplete="email"
         />
+        
+        <select
+          name="userType"
+          required
+          className="p-2 m-2 box-border rounded-lg text-sm flex-1"
+          value={userType}
+          onChange={(e) => setUserType(e.target.value)}
+          autoComplete="userType" style={{color: 'black'}}
+        >
+          <option value="fitstar" style={{color: 'black'}}>FitStar</option>
+          <option value="fitguide" style={{color: 'black'}}>FitGuide</option>
+        </select>
+
         {error ? <div className="text-red-500">{error}</div> : null}
+        
         <button
           type="submit"
-          className="m-2 p-3 box-border border-none rounded-lg text-lg flex-none bg-[#95BF1D] hover:bg-[#6e862a] text-white"
+          className="box-border border-none rounded-lg text-md bg-[#95BF1D] hover:bg-[#6e862a] text-white"
         >
-          Join Waitlist
+          JOIN WAITLIST
         </button>
       </form>
     </>
