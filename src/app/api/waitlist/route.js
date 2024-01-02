596623b799810a2b773d7e537d2006ff5749f20d
Maggie Muriithi <muriithinyawira13@gmail.com>
2024-01-02 14:22:30 +0300

    Airtable form incorporated

--- src/app/api/waitlist/route.js
+++ src/app/api/waitlist/route.js
@@ -18,3 +18,37 @@ export async function POST(req) {
   }
   return NextResponse.json({ ok: true }, { status: 200 });
 }
+
+/* export async function POST(req) {
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    process.env.AIRTABLE_BASE_ID
+  );
+
+  base(process.env.AIRTABLE_TABLE_NAME)
+    .select({
+      // Selecting the first 3 records in Grid, Filtered by Name:
+      maxRecords: 3,
+      view: "Grid, Filtered by Name",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+          console.log("Retrieved", record.get("Name"));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+  return NextResponse.json({ ok: true }, { status: 200 });
+} */
--- src/components/WaitForm.jsx
+++ src/components/WaitForm.jsx
@@ -23,31 +23,43 @@ export default function Form() {
   if (hasSubmitted) {
     return (
       <div className="p-8">
-
         <div className="form-submit">
-      
-          <Image src='/images/Frame (1).png' alt='' width='50' height='50' />
+          <Image src="/images/Frame (1).png" alt="" width="50" height="50" />
           <p>Your interest has been successfully received!</p>
           <p>You will be notified duly!</p>
           <p>Anticipate our launch in 2024!</p>
-      
-          <div className='navlinksa' style={{width: '30%', marginTop: '1em'}}>
-      
-            <div className="flex flex-wrap gap-2">
-      
-              <Image src='/images/X svg.png' alt='' width='30' height='30' />
-              <Image src='/images/Facebook svg.png' alt='' width='30' height='30' />
-              <Image src='/images/Instagram svg.png' alt='' width='30' height='30' />
-              <Image src='/images/Linkedin svg.png' alt='' width='30' height='30' />
-      
-            </div>
-      
-            <p className="text-center"style={{color: 'black', fontSize: '15px'}}>connect with Us</p>
-      
-          </div>
-          
-        </div>
 
+          <div className="navlinksa" style={{ width: "30%", marginTop: "1em" }}>
+            <div className="flex flex-wrap gap-2">
+              <Image src="/images/X svg.png" alt="" width="30" height="30" />
+              <Image
+                src="/images/Facebook svg.png"
+                alt=""
+                width="30"
+                height="30"
+              />
+              <Image
+                src="/images/Instagram svg.png"
+                alt=""
+                width="30"
+                height="30"
+              />
+              <Image
+                src="/images/Linkedin svg.png"
+                alt=""
+                width="30"
+                height="30"
+              />
+            </div>
+
+            <p
+              className="text-center"
+              style={{ color: "black", fontSize: "15px" }}
+            >
+              connect with Us
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
@@ -55,43 +67,38 @@ export default function Form() {
   return (
     <>
       <form ref={form} className="waitform" onSubmit={submit}>
+        <iframe
+          class="airtable-embed"
+          src="https://airtable.com/embed/apppGAZzFpZRpbS9T/pagqJcttqPaZe4vpY/form"
+          frameborder="0"
+          onmousewheel=""
+          width="100%"
+          height="533"
+          style={{background: "transparent"}}
+        ></iframe>
 
-        <input
-          type="text"
-          name="fullName"
-          required
-          placeholder="Enter your Full Name"
-          className="p-2 m-2 box-border rounded-lg text-sm flex-1 text-black"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          autoComplete="name"
-        />
-        <input
-          type="email"
-          name="email"
-          required
-          placeholder="Enter your Email Address"
-          className="p-2 m-2 box-border rounded-lg text-sm flex-1 text-black"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="email"
-        />
-        
         <select
           name="userType"
           required
           className="p-2 m-2 box-border rounded-lg text-sm flex-1"
           value={userType}
           onChange={(e) => setUserType(e.target.value)}
-          autoComplete="userType" style={{color: 'black'}}
+          autoComplete="userType"
+          style={{ color: "black" }}
         >
-          <option value="fitstar" style={{color: 'black'}} disabled>User type</option>
-          <option value="fitstar" style={{color: 'black'}}>FitStar</option>
-          <option value="fitguide" style={{color: 'black'}}>FitGuide</option>
+          <option value="fitstar" style={{ color: "black" }} disabled>
+            User type
+          </option>
+          <option value="fitstar" style={{ color: "black" }}>
+            FitStar
+          </option>
+          <option value="fitguide" style={{ color: "black" }}>
+            FitGuide
+          </option>
         </select>
 
         {error ? <div className="text-red-500">{error}</div> : null}
-        
+
         <button
           type="submit"
           className="box-border border-none rounded-lg text-md bg-[#95BF1D] hover:bg-[#6e862a] text-white"
