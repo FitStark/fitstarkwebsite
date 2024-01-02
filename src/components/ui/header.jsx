0ce7e57a9c1d418747257afd9952723ed5567700
Maggie Muriithi <muriithinyawira13@gmail.com>
2024-01-02 16:14:41 +0300

    fixed?

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="bg-transparent fixed z-10 w-full sm:hidden md:hidden">
+    <header className="bg-transparent fixed z-10 w-full block">
       <div className=" mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
           {/* Site branding */}
