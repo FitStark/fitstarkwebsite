6a4ee989b666efcc7c5520c415147b151b561f3d
Maggie Muriithi <muriithinyawira13@gmail.com>
2024-01-04 12:20:36 +0300

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <Head>
+        <Link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
